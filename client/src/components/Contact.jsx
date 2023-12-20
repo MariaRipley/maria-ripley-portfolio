a55c4c7b6b794e443c0a68b7497971a9e123d9da
MariaRipley <roblesb.maria@gmail.com>
2023-12-20 17:44:18 +0100

    Create contact form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,9 +3,36 @@ import "../styles/Contact.scss";
 
 function Contact() {
   return (
-    <>
-      <h1>CONTACT</h1>
-    </>
+    <div className="contact">
+      <h1 className="contact__title">CONTACT</h1>
+      <form className="contact__form" method="POST" autoComplete="off">
+        <label className="contact__form-label">Name</label>
+        <input
+          className="contact__form-input"
+          type="text"
+          placeholder="Your name here..."
+        />
+
+        <label className="contact__form-label">Email</label>
+        <input
+          className="contact__form-input"
+          type="email"
+          placeholder="Your email here..."
+        />
+
+        <label className="contact__form-label">Message</label>
+        <textarea
+          className="contact__form-input contact__form-input--message"
+          placeholder="Your message here..."
+        ></textarea>
+
+        <input
+          className="contact__form-input contact__form-input--submit"
+          type="submit"
+          value="Send"
+        />
+      </form>
+    </div>
   );
 }
 
