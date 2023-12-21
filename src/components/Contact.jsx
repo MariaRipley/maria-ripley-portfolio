110220854fd1c97514e9f9aa6e25ecdcbf3c7c97
MariaRipley <roblesb.maria@gmail.com>
2023-12-21 17:49:07 +0100

    Add Footer to About page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,14 @@ import "../styles/Contact.scss";
 
 //Components
 import Form from "./Form";
+import Footer from "./Footer";
 
 function Contact() {
   return (
     <div className="contact">
       <h1 className="contact__title">CONTACT</h1>
       <Form />
+      <Footer />
     </div>
   );
 }
