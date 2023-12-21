4c29c27e68d6cc74f385fa34ac6a2354a28e85a1
MariaRipley <roblesb.maria@gmail.com>
2023-12-21 17:45:37 +0100

    Add Form component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,51 +1,14 @@
 //Styles
 import "../styles/Contact.scss";
 
+//Components
+import Form from "./Form";
+
 function Contact() {
   return (
     <div className="contact">
       <h1 className="contact__title">CONTACT</h1>
-      <form
-        data-netlify="true"
-        className="contact__form"
-        method="POST"
-        autoComplete="off"
-        name="contact"
-        action="/contact"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <label className="contact__form-label">Name</label>
-        <input
-          className="contact__form-input"
-          type="text"
-          name="name"
-          placeholder="Your name here..."
-          required
-        />
-
-        <label className="contact__form-label">Email</label>
-        <input
-          className="contact__form-input"
-          type="email"
-          name="email"
-          placeholder="Your email here..."
-          required
-        />
-
-        <label className="contact__form-label">Message</label>
-        <textarea
-          className="contact__form-input contact__form-input--message"
-          name="message"
-          placeholder="Your message here..."
-          required
-        ></textarea>
-
-        <input
-          className="contact__form-input contact__form-input--submit"
-          type="submit"
-          value="Send"
-        />
-      </form>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,49 @@
+
+
+function Form() {
+  return (
+    <form
+      data-netlify="true"
+      className="contact__form"
+      method="POST"
+      autoComplete="off"
+      name="contact"
+      action="/contact"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <label className="contact__form-label">Name</label>
+      <input
+        className="contact__form-input"
+        type="text"
+        name="name"
+        placeholder="Your name here..."
+        required
+      />
+
+      <label className="contact__form-label">Email</label>
+      <input
+        className="contact__form-input"
+        type="email"
+        name="email"
+        placeholder="Your email here..."
+        required
+      />
+
+      <label className="contact__form-label">Message</label>
+      <textarea
+        className="contact__form-input contact__form-input--message"
+        name="message"
+        placeholder="Your message here..."
+        required
+      ></textarea>
+
+      <input
+        className="contact__form-input contact__form-input--submit"
+        type="submit"
+        value="Send"
+      />
+    </form>
+  );
+}
+
+export default Form;
