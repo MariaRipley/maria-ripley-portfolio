a1f0da8731489d2f5e558419cd380eb1b3c7a015
MariaRipley <roblesb.maria@gmail.com>
2023-12-21 17:06:11 +0100

    Add data-netlify=tru to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,19 @@ function Contact() {
   return (
     <div className="contact">
       <h1 className="contact__title">CONTACT</h1>
-      <form className="contact__form" method="POST" autoComplete="off">
+      <form
+        data-netlify="true"
+        className="contact__form"
+        method="POST"
+        autoComplete="off"
+        name="contact"
+        action="/contact"
+      >
         <label className="contact__form-label">Name</label>
         <input
           className="contact__form-input"
           type="text"
+          name="name"
           placeholder="Your name here..."
         />
 
@@ -17,12 +25,14 @@ function Contact() {
         <input
           className="contact__form-input"
           type="email"
+          name="email"
           placeholder="Your email here..."
         />
 
         <label className="contact__form-label">Message</label>
         <textarea
           className="contact__form-input contact__form-input--message"
+          name="message"
           placeholder="Your message here..."
         ></textarea>
 
