1d659088f90e936f350992426ed1daf9309a7309
MariaRipley <roblesb.maria@gmail.com>
2024-01-11 11:30:14 +0100

    Fix portfolio desktop version and footer mobile version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ function Footer() {
   return (
     <div className="footer">
       <p className="footer__info">
-        María Robles Web Developer | 2023 | Contact me
+        María Robles Web Developer | 2024 |{" "}
+        <a className="footer__info footer__info--link" href="">
+          Contact me
+        </a>
       </p>
     </div>
   );
