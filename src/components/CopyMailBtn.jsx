81ac0d3356badfa10d76ec121ad31033b10c302d
MariaRipley <roblesb.maria@gmail.com>
2024-01-11 18:16:52 +0100

    Add Copy Email button styles and fix issue with copied text

--- src/components/CopyMailBtn.jsx
+++ src/components/CopyMailBtn.jsx
@@ -1,22 +1,21 @@
-//React
+// React
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Toaster, toast } from "react-hot-toast";
 
-//Styles
+// Styles
 import "../styles/CopyMailBtn.scss";
 
 function CopyMailBtn() {
+  const handleCopyClick = () => {
+    toast.success("Text copied successfully");
+  };
+
   return (
     <>
       <CopyToClipboard text="roblesb.maria@gmail.com">
-        <>
-          <button
-            className="info__mail"
-            onClick={() => toast("Text copied successfully")}
-          >
-            Copy my Email
-          </button>
-        </>
+        <button className="info__mail" onClick={handleCopyClick}>
+          Copy my Email
+        </button>
       </CopyToClipboard>
       <Toaster />
     </>
